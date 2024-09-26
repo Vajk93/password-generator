7b7b8819f8308c68fc75789dd18bb1347a05f786
Vajk93 <67112672+Vajk93@users.noreply.github.com>
2024-09-26 19:06:31 +0200

    [MAJOR] set up colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-red": "#F64A4A",
+        "app-orange": "#FB7C58",
+        "app-yellow": "#F8CD65",
+        "app-green": "#A4FFAF",
+        "app-white": "#E6E5EA",
+        "app-grey": "#817D92",
+        "app-dark-grey": "#24232C",
+        "app-very-dark-grey": "#18171F",
+      }
+    },
   },
   plugins: [],
 }
